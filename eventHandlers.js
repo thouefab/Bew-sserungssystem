f3b107914a128a93dd8185f417fdbef42c1e442d
thouefab <thouefab@students.zhaw.ch>
2021-12-18 22:08:28 +0100

    commit on Master

--- eventHandlers.js
+++ eventHandlers.js
@@ -4,6 +4,7 @@ exports.sendEvent = null;
 
 exports.registerEventHandlers = function (source) {
     source.addEventListener('Lux', forwardEvent);
+    source.addEventListener('Feuchtigkeit', forwardEvent);
     // Register more event handlers here
 }
 
@@ -18,7 +19,9 @@ function forwardEvent(event) {
 
     try {        
         // Log the event in the database
-        logger.logOne("MyDB", "Lux", data);
+        logger.logOne("MyDB", event.type, data);
+        
+        
 
         // send data to all connected clients
         exports.sendEvent(data);
@@ -27,3 +30,23 @@ function forwardEvent(event) {
         console.log(error)
     }
 }
+/*function forwardEvent1(event) {
+    // read variables from the event
+    var data1 = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data, // the value of the event
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    try {        
+        // Log the event in the database
+        logger.logMany("MyDB", "Feuchtigkeit", data1);
+
+        // send data to all connected clients
+        exports.sendEvent(data1);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}*/
--- public/visualizationtwo.js
+++ public/visualizationtwo.js
@@ -16,7 +16,9 @@ initSSE();
 
 
 // Array, in dem alle empfangenen Lux-Werte gespeichert werden.
-var allMeasurements = [];
+var LuxMeasurements = [];
+var FeuchtigkeitMeasurements = [];
+
 
 // Maximaler Lux Level für die Berechnung des Prozentwerts und als maximaler Wert für das Chart.
 // -- TODO Aufgabe 1 -- 
@@ -32,7 +34,7 @@ function updateVariables(data) {
         //console.log(lux);
 
         // Wert am Ende des Arrays 'allMeasurements' hinzufügen
-        allMeasurements.push(lux);
+        LuxMeasurements.push(lux);
 
         // Wert in Prozent umrechnen und in 'level' speichern
         var level = lux * (100 / maxLevel);
@@ -62,10 +64,10 @@ function updateVariables(data) {
 
         // Durchschnitt aller bisherigen Messungen berechnen und in 'luxAverage' speichern
         var luxSum = 0;
-        for (var measurement of allMeasurements) {
+        for (var measurement of LuxMeasurements) {
             luxSum = luxSum + measurement;
         }
-        var luxAverage = luxSum / allMeasurements.length;
+        var luxAverage = luxSum / LuxMeasurements.length;
         //console.log(luxAverage);
 
         // -- TODO Aufgabe 3 -- 
@@ -75,67 +77,66 @@ function updateVariables(data) {
         // Wert im Chart hinzufügen
         addData(lux);
     }
-
+    
     //Feuchtigkeitslevelbar
 
-if (data.eventName === "Feuchtigkeit") {
-    // Erhaltenen Wert in der Variable 'lux' speichern
-    var feuchtigkeit = Number(data.eventData1);
-    //console.log(lux);
+    if (data.eventName === "Feuchtigkeit") {
+        // Erhaltenen Wert in der Variable 'lux' speichern
+        var feuchtigkeit = Number(data.eventData);
+     //console.log(lux);
 
-    // Wert am Ende des Arrays 'allMeasurements' hinzufügen
-    allMeasurements.push(feuchtigkeit);
+        // Wert am Ende des Arrays 'allMeasurements' hinzufügen
+     FeuchtigkeitMeasurements.push(feuchtigkeit);
 
-    // Wert in Prozent umrechnen und in 'level' speichern
-    var level = feuchtigkeit * (100 / maxLevel);
+        // Wert in Prozent umrechnen und in 'level' speichern
+        var level = feuchtigkeit * (100 / maxLevel);
 
-    // Farbe des Balkens abhängig von Level festlegen
-    // Liste aller unterstützten Farben: https://www.w3schools.com/cssref/css_colors.asp
-    // -- TODO Aufgabe 2 -- 
-    // Weitere Farben abhängig vom Level
-    if (level <= 30) {
-        color = "Green";
-    } else {
-        color = "Orange";
-    }
+        // Farbe des Balkens abhängig von Level festlegen
+        // Liste aller unterstützten Farben: https://www.w3schools.com/cssref/css_colors.asp
+        // -- TODO Aufgabe 2 -- 
+        // Weitere Farben abhängig vom Level
+        if (level <= 30) {
+            color = "Green";
+        } else {
+           color = "Orange";
+        }
 
-    // CSS Style für die Hintergrundfarbe des Balkens
-    var colorStyle = "background-color: " + color + " !important;";
+        // CSS Style für die Hintergrundfarbe des Balkens
+        var colorStyle = "background-color: " + color + " !important;";
 
-    // CSS Style für die Breite des Balkens in Prozent
-    var widthStyle = "width: " + level1 + "%;"
+        // CSS Style für die Breite des Balkens in Prozent
+        var widthStyle = "width: " + level + "%;"
 
-    // Oben definierte Styles für Hintergrundfarbe und Breite des Balkens verwenden, um
-    // den Progressbar im HTML-Dokument zu aktualisieren
-    document.getElementById("feuchtigkeit-bar").style = colorStyle1 + widthStyle1;
+        // Oben definierte Styles für Hintergrundfarbe und Breite des Balkens verwenden, um
+        // den Progressbar im HTML-Dokument zu aktualisieren
+        document.getElementById("feuchtigkeit-bar").style = colorStyle + widthStyle;
 
-    // Text unterhalb des Balkens aktualisieren
-    document.getElementById("feuchtigkeit-text").innerHTML = feuchtigkeit + "Feuchtigkeit"
+        // Text unterhalb des Balkens aktualisieren
+        document.getElementById("feuchtigkeit-text").innerHTML = "Feuchtigkeit: " + feuchtigkeit + " %"
 
-    // Durchschnitt aller bisherigen Messungen berechnen und in 'luxAverage' speichern
-    var feuchtigkeitSum = 0;
-    for (var measurement of allMeasurements) {
-        feuchtigkeitSum = feuchtigkeitSum + measurement;
-    }
-    var feuchtigkeitAverage = feuchtigkeitSum / allMeasurements.length;
-    //console.log(luxAverage);
+        // Durchschnitt aller bisherigen Messungen berechnen und in 'luxAverage' speichern
+        var feuchtigkeitSum = 0;
+        for (var measurement of FeuchtigkeitMeasurements) {
+            feuchtigkeitSum = feuchtigkeitSum + measurement;
+        }
+        var feuchtigkeitAverage = feuchtigkeitSum / FeuchtigkeitMeasurements.length;
+        //console.log(luxAverage);
 
-    // -- TODO Aufgabe 3 -- 
-    // Durchschnittlichen Lux-Wert (luxAverage) in Prozent umrechnen und als Balken und Text anzeigen
+        // -- TODO Aufgabe 3 -- 
+        // Durchschnittlichen Lux-Wert (luxAverage) in Prozent umrechnen und als Balken und Text anzeigen
     
 
-    // Wert im Chart hinzufügen
-    addData1(feuchtigkeit);
+        // Wert im Chart hinzufügen
+        addData1(feuchtigkeit);
+    }
 }
-}
-
 
 //////////////////////////////////
 /////   Code für das Chart 1  ///// 
 //////////////////////////////////
 
 // Chart und Variablen 
-var chartData, chartOptions, chart;
+var chartData, chartData1, chartOptions, chartOptions1, chart, chart1;
 google.charts.load('current', { packages: ['corechart'] });
 google.charts.setOnLoadCallback(drawChart);
 
@@ -145,6 +146,7 @@ function drawChart() {
     // (Dieser Dummy-Wert ist nötig, damit wir das Chart schon anzeigen können, bevor 
     // wir Daten erhalten. Es können keine Charts ohne Daten gezeichnet werden.)
     chartData = new google.visualization.arrayToDataTable([['Time', 'Lux'], ["", 0]]);
+    
     // Chart Options festlegen
     chartOptions = {
         title: 'Lux Level',
@@ -164,10 +166,37 @@ function drawChart() {
             },
         }
     };
+   
+    
     // LineChart initialisieren
-    chart = new google.visualization.LineChart(document.getElementById('luxlevel-chart'));
+    chart = new google.visualization.LineChart(document.getElementById('lux-chart'));
     chartData.removeRow(0); // Workaround: ersten (Dummy-)Wert löschen, bevor das Chart zum ersten mal gezeichnet wird.
     chart.draw(chartData, chartOptions); // Chart zeichnen
+
+    chartData1 = new google.visualization.arrayToDataTable([['Time', 'Feuchtigkeit'], ["", 0]]);
+    chartOptions1 = {
+        title: 'Feuchtigkeit Level',
+        hAxis: { title: 'Time' },
+        vAxis: { title: 'Feuchtigkeit' },
+        animation: {
+            duration: 300, // Dauer der Animation in Millisekunden
+            easing: 'out',
+        },
+        curveType: 'function', // Werte als Kurve darstellen (statt mit Strichen verbundene Punkte)
+        legend: 'none',
+        vAxis: {
+            // Range der vertikalen Achse
+            viewWindow: {
+                min: 0,
+                max: maxLevel
+            },
+        }
+    };
+
+    // LineChart initialisieren
+    chart1 = new google.visualization.LineChart(document.getElementById('feuchtigkeit-chart'));
+    chartData1.removeRow(0); // Workaround: ersten (Dummy-)Wert löschen, bevor das Chart zum ersten mal gezeichnet wird.
+    chart1.draw(chartData1, chartOptions1); // Chart zeichnen
 }
 
 // Eine neuen Wert ins Chart hinzufügen
@@ -189,59 +218,7 @@ function addData(lux) {
     chart.draw(chartData, chartOptions);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////
-/////   Code für das Chart 2  ///// 
-//////////////////////////////////
-// Chart und Variablen 
-var chartData1, chartOptions1, chart1;
-google.charts.load('current', { packages: ['corechart'] });
-google.charts.setOnLoadCallback(drawChart1);
-
-// Chart initialisieren. Diese Funktion wird einmalig aufgerufen, wenn die Page geladen wurde.
-function drawChart1() {
-    // Daten mit dem Dummy-Wert ["", 0] initialisieren. 
-    // (Dieser Dummy-Wert ist nötig, damit wir das Chart schon anzeigen können, bevor 
-    // wir Daten erhalten. Es können keine Charts ohne Daten gezeichnet werden.)
-    chartData1 = new google.visualization.arrayToDataTable([['Time', 'Feuchtigkeit'], ["", 0]]);
-    // Chart Options festlegen
-    chartOptions1 = {
-        title: 'Feuchtigkeit Level',
-        hAxis: { title: 'Time' },
-        vAxis: { title: 'Feuchtigkeit' },
-        animation: {
-            duration: 300, // Dauer der Animation in Millisekunden
-            easing: 'out',
-        },
-        curveType: 'function', // Werte als Kurve darstellen (statt mit Strichen verbundene Punkte)
-        legend: 'none',
-        vAxis: {
-            // Range der vertikalen Achse
-            viewWindow: {
-                min: 0,
-                max: maxLevel
-            },
-        }
-    };
-    // LineChart initialisieren
-    chart1 = new google.visualization.LineChart(document.getElementById('feuchtigkeitslevel-chart1'));
-    chartData1.removeRow(0); // Workaround: ersten (Dummy-)Wert löschen, bevor das Chart zum ersten mal gezeichnet wird.
-    chart1.draw(chartData1, chartOptions1); // Chart zeichnen    
-}
-
-// Eine neuen Wert ins Chart hinzufügen
-function addData(feuchtigkeit) {
+function addData1(feuchtigkeit) {
 
     // -- TODO Aufgabe 4 --
     // Nur die letzten 10 gemessenen Werte anzeigen.
@@ -258,3 +235,16 @@ function addData(feuchtigkeit) {
     // Chart neu rendern
     chart1.draw(chartData1, chartOptions1);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
